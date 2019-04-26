b24c681ac3d816cae9adf3bab20490a5d44de717
Yanis DANDO <yanis.dando@gmail.com>
2019-04-26 16:44:58 +0200

    updating

--- 8/tensorflou.py
+++ 8/tensorflou.py
@@ -119,7 +119,7 @@ def main():
     plt.plot(epochs, acc, 'bo', label='Training acc')
     plt.plot(epochs, val_acc, 'b', label='Validation acc')
     plt.title('Training and validation accuracy')
-    plt.xlabel('Epochs')
+    plt.xlabel('Iterations')
     plt.ylabel('Accuracy')
     plt.legend()
 
